c5fd2c9de76ede428250e79f165eb85613e27fa3
Ben <ben.sisney@gmail.com>
2024-05-18 07:26:29 -0500

    Update test_PIR_HC-SR501.py

--- functionalTests/test_PIR_HC-SR501.py
+++ functionalTests/test_PIR_HC-SR501.py
@@ -1,1 +1,9 @@
+import datetime
+from gpiozero import MotionSensor
 
+pir = MotionSensor(21)
+
+while True:
+        pir.wait_for_motion()
+        print(f'{datetime.datetime.now()} - Motion Detected')
+        pir.wait_for_no_motion()
