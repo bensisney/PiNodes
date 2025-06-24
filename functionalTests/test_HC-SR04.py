730a1ad33189ec4ebb6223f420432504855b56d2
Ben <ben.sisney@gmail.com>
2025-06-24 02:45:37 -0500

    Update test_HC-SR04.py

--- functionalTests/test_HC-SR04.py
+++ functionalTests/test_HC-SR04.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(echo=24, trigger=23)
+sensor = DistanceSensor(echo=24, trigger=23, max_distance=4)
 while True:
     print('Distance: ', sensor.distance * 100)
     sleep(1)
